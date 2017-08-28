875b2c63da306019e5fe279fab7331e55f06f7ba
lucask95 <lucaskeller1995@gmail.com>
2017-08-28 16:25:38 -0700

    Changed input style to make life easier.

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-    $("#goBtn").click(insertTime);
-    $("#timeIn").keypress(function(e) { if (e.keyCode == 13) insertTime(); });
-});
-
 class RunTime {
     constructor() {
         this.times = [];
@@ -22,22 +17,27 @@ class RunTime {
     }
 }
 
-// should be able to process inputs such as 1h12m5s100ms, 5m2s, 1:12:05.1, 5:02
+var runTimes = {};
+
+// should be able to process inputs in HH:MM:SS format, may omit HH: or HH:MM
 function processInput(inputString) {
     var timeInMs = 0;
-    var delims = ['h', 'm', 's', 'ms', ':'];
-    var timeNumbers = [];
-
-    for (var i = delims.length - 1; i >= 0; i--) {
-        var splitString = inputString.split(delims[i]);
-        while (splitString.length > 0) {
-            var tempString = splitString.pop();
-        }
-    }
-
+    var splitString = inputString.split(':');
     return timeInMs;
 }
 
 function insertTime() {
     var inputString = $("#timeIn").val();
+    var newTime = processInput(inputString);
 }
+
+function initialize() {
+    runTimes = new RunTime();
+}
+
+$(document).ready(function(){
+    processInput("1h12m5s");
+    initialize();
+    $("#goBtn").click(insertTime);
+    $("#timeIn").keypress(function(e) { if (e.keyCode == 13) insertTime(); });
+});
