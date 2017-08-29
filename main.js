f8f34c5b6fc0d42b38b4f3c34da50516954c5e53
lucask95 <lucaskeller1995@gmail.com>
2017-08-29 09:33:43 -0700

    Implemented most of the RunTime class.

--- main.js
+++ main.js
@@ -6,15 +6,60 @@ class RunTime {
         this.mean = -1;
         this.median = -1;
         this.mode = -1;
+
+        // extra variables for helping calculate values
+        this.occurrances = {};
+        this.timesSum = 0;
+    }
+
+    findMax() {
+        var newTime = this.times[this.times.length-1];
+        if (newTime > this.max)
+            this.max = newTime;
+    }
+
+    findMin() {
+        var newTime = this.times[this.times.length-1];
+        if (this.min == -1 || newTime < this.min)
+            this.min = newTime;
+    }
+
+    calcMean() {
+        this.mean = this.timesSum / this.times.length;
+    }
+
+    findMode() {
+        var newTime = this.times[this.times.length-1];
+
+        // update key value pair
+        if(this.occurrances[newTime] == undefined)
+            this.occurrances[newTime] = 1;
+        else
+            this.occurrances[newTime]++;
+
+        // check number of times newest time has occurred. if it has occurred
+        // more times than current mode, replace current mean
+        if (this.mode == -1 || this.occurrances[newTime] > this.occurrances[this.mode])
+            this.mode = newTime;
+    }
+
+    findMedian() {
+        this.times.sort();
+        this.median = Math.floor(this.times.length);
+    }
+
+    calcAll() {
+        this.findMax();
+        this.findMin();
+        this.calcMean();
+        this.findMode();
+        this.findMedian();
     }
 
     push(n) {
         this.times.push(n);
-        calcAll();
-    }
-
-    calcAll() {
-
+        this.timesSum += n;
+        this.calcAll();
     }
 }
 
@@ -28,6 +73,10 @@ function processInput(inputString) {
     for (var i = 0; splitString.length > 0; i++)
         timeInSec += Number(splitString.pop()) * Math.pow(60, i);
     return timeInSec;
+}
+
+function updateInfo() {
+
 }
 
 function insertTime() {
